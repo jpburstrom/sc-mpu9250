203069f3ab09490ac242f221eea7646802050f93
Alex Wilson <aewilson@ualberta.ca>
2015-02-20 21:06:54 +0000

    Fix bugs in app:debug/main.cpp
     * have to call begin()!!
     * add some spacing for better readability

--- apps/debug/main.cpp
+++ apps/debug/main.cpp
@@ -21,6 +21,7 @@ int main(int argc, char** argv) {
   sigaction(SIGINT, &sigIntHandler, NULL);
 
   Adafruit_LSM9DS0 lsm;
+  lsm.begin();
   lsm.setupAccel(lsm.LSM9DS0_ACCELRANGE_2G);
   lsm.setupMag(lsm.LSM9DS0_MAGGAIN_2GAUSS);
   lsm.setupGyro(lsm.LSM9DS0_GYROSCALE_245DPS);
@@ -30,13 +31,13 @@ int main(int argc, char** argv) {
     std::cout << "Accel X: " << lsm.accelData.x
        << " Y: " << (int)lsm.accelData.y
        << " Z: " << lsm.accelData.z
-       << "Mag X: " << lsm.magData.x
+       << "\t Mag X: " << lsm.magData.x
        << " Y: " << lsm.magData.y
        << " Z: " << lsm.magData.z
-       << "Gyro X: " << lsm.gyroData.x
+       << "\t Gyro X: " << lsm.gyroData.x
        << " Y: " << lsm.gyroData.y
        << " Z: " << lsm.gyroData.z
-       << "Temp: "  << lsm.temperature << std::endl;
+       << "\t Temp: "  << lsm.temperature << std::endl;
 
     delay(200);
   }
