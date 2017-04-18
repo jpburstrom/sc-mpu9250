cad185d74b645a1de8bdc9df76b27c2c549c7d99
Johannes Burström <johannes@ljud.org>
2017-04-18 12:33:42 +0000

    Calibration bump

--- apps/calibrate/main.cpp
+++ apps/calibrate/main.cpp
@@ -22,6 +22,32 @@ int main(int argc, char** argv) {
 
     MPU9250 mpu;
 
+    float selfTest[6];
+
+    char userInput;
+
+    mpu.MPU9250SelfTest(selfTest); // Start by performing self test and reporting values
+    printf("x-axis self test: acceleration trim within %f%% of factory value\n", selfTest[0]);
+    printf("y-axis self test: acceleration trim within %f%% of factory value\n", selfTest[1]);
+    printf("z-axis self test: acceleration trim within %f%% of factory value\n", selfTest[2]);
+    printf("x-axis self test: gyration trim within %f%% of factory value\n", selfTest[3]);
+    printf("y-axis self test: gyration trim within %f%% of factory value\n", selfTest[4]);
+    printf("z-axis self test: gyration trim within %f%% of factory value\n", selfTest[5]);
+    
+    bool warn=false;
+    for (int i=0; i < 6; i++) {
+        if (fabs(selfTest[i]) > 14) {
+            warn = true;
+        }
+    }
+
+    if (warn) {
+        printf("WARNING: Some percentages seem to be a bit off.\n");
+    } else {
+        printf("Seems ok.\n");
+    }
+
+    usleep(1e06);
 
     if (!mpu.init()) {
         std::cerr <<  "sensor init failed!" << std::endl;
@@ -39,27 +65,34 @@ int main(int argc, char** argv) {
 
 
 
+    //Calibrate accel + gyro
     cout << "Calibrating Accel + Gyro\n";
-
     mpu.calibrateAccelGyro();
-    
     printf("Accel bias: %f, %f, %f\n", mpu.accel.bias[0], mpu.accel.bias[1], mpu.accel.bias[2]) ;
     printf("Gyro bias: %f, %f, %f\n", mpu.gyro.bias[0], mpu.gyro.bias[1], mpu.gyro.bias[2]) ;
 
+    mpu.init(); // reset various stuff
+
+
+    printf("Calibrate magnetometer? (y/n)");
+    scanf("%c", &userInput);
+
+    if (userInput == 'y') {
+   
+        cout << "Calibrating Magnetometer\n";
+        cout << "Wave device in a figure eight until done!\n";
+        mpu.calibrateMag();
+        printf("Mag bias: %f, %f, %f\n", mpu.mag.bias[0], mpu.mag.bias[1], mpu.mag.bias[2]) ;
+        printf("Mag scale: %f, %f, %f\n", mpu.mag.scale[0], mpu.mag.scale[1], mpu.mag.scale[2]) ;
+    
+    }
 
     while (running) {
         mpu.read();
-        std::cout << "Accel X: " << mpu.accel.x
-            << " Y: " << mpu.accel.y
-            << " Z: " << mpu.accel.z
-            << "\t Mag X: " << mpu.mag.x
-            << " Y: " << mpu.mag.y
-            << " Z: " << mpu.mag.z
-            << "\t Gyro X: " << mpu.gyro.x
-            << " Y: " << mpu.gyro.y
-            << " Z: " << mpu.gyro.z
-            << "\t Temp: "  << mpu.temperature << std::endl;
+        printf("\rAccel X: %.02f\t Accel Y: %.02f\t Accel Z: %.02f\t Pitch: %.02f\t Roll: %.02f\t Yaw: %.02f", 
+                mpu.accel.x, mpu.accel.y, mpu.accel.z, mpu.orientation.pitch, mpu.orientation.roll, mpu.orientation.yaw);
 
-        usleep(100000);
+        fflush(stdout);
+        usleep(10000);
     }
 }
