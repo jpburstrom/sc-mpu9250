dea58898b351117138de063c9719ae3250a32d73
Johannes Burström <johannes@ljud.org>
2018-08-20 12:41:03 +0000

    Remove unneeded libs

--- libs/Adafruit_AHRS/README.md
+++
@@ -1,1 +0,0 @@
-Copied from https://github.com/adafruit/Adafruit_AHRS (revision 0db89bfd3a) 
--- libs/Adafruit_Sensor/Adafruit_Sensor.cpp
+++
@@ -1,7 +0,0 @@
-#include "Adafruit_Sensor.h"
-
-void Adafruit_Sensor::constructor() {
-}
-
-Adafruit_Sensor::~Adafruit_Sensor() {
-}
